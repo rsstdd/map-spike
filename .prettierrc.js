e0aa90722f7f5b32b785015717d4804578e5be2f
Ross Todd <rosstod@gmail.com>
2019-02-23 16:03:59 -0800

    style(rc): Add .prettier and linting configs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  'max-len': 80,
+};
